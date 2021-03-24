176ca2b2229d32f821d6d807f9fc69a192a232a7
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2021-03-23 22:11:46 -0300

    Introducing forks for multi-client connections

--- server.c
+++ server.c
@@ -21,6 +21,8 @@ int main()
     char str[MESSAGE_SIZE] = "";
 	char server_database[SERVER_SIZE];
 
+	pid_t childpid;
+
 	/* Inicializar server_database */         
 	for (int i = 0; i < SERVER_SIZE; i++) {
 		server_database[i] = 'T';
@@ -32,7 +34,6 @@ int main()
 	server_database[5] = '5';
 
 
-
 	/* Inicialização de sockets */         
 	struct sockaddr_in address;
 	server_sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -42,65 +43,72 @@ int main()
 	bind(server_sockfd, (struct sockaddr*)&address , sizeof(address));
 	listen(server_sockfd , 20);
  
-	/* Esperando um unico cliente uma unica vez */         
-	printf("\nServer rodando, aguardando contato com cliente\n");
-	fflush(stdout);
-	client_sockfd = accept(server_sockfd, (struct sockaddr*)NULL, NULL);
-	printf("\nCliente conectado\n");
-	fflush(stdout);
-	while(1)
-	{
-		/* Operações pós conexão */         
-
-		// assumindo "ler" e "sair"
-		// 
-
-        read(client_sockfd, &str, MESSAGE_SIZE);
-		// printf("%s\n", str);
-		// printf("%d\n", strlen(str)+1);
-		printf("\nRecebemos: %s\n", str);
+	while (1) {
+		/* Esperando um unico cliente uma unica vez */         
+		printf("\nServer rodando, aguardando contato com cliente\n");
+		fflush(stdout);
+		client_sockfd = accept(server_sockfd, (struct sockaddr*)NULL, NULL);
+		printf("\nCliente conectado\n");
 		fflush(stdout);
 
-		if(strcmp(str,"sair")==0){
-            break;
+		if ((childpid = fork()) == 0) {
+			// close(sockfd); // vi um tcho fazendo isso
+
+			while(1)
+			{
+				/* Operações pós conexão */         
+
+				// assumindo "ler" e "sair"
+				// 
+
+				read(client_sockfd, &str, MESSAGE_SIZE);
+				// printf("%s\n", str);
+				// printf("%d\n", strlen(str)+1);
+				printf("\nRecebemos: %s\n", str);
+				fflush(stdout);
+
+				if(strcmp(str,"sair")==0){
+					break;
+				}
+
+				// [ ] - Le(Posicao4, tamanho)
+				char starting_position_arg[100] = "";
+				char size_arg[100] = "";
+				int starting_position, size;
+
+				read(client_sockfd, &starting_position_arg, 100);
+				printf("\n starting_position_arg: %s\n", starting_position_arg);
+				fflush(stdout);
+
+				read(client_sockfd, &size_arg, 100);
+				printf("\n size_arg: %s\n", size_arg);
+				fflush(stdout);
+
+				starting_position = atoi(starting_position_arg);
+				size = atoi(size_arg);
+
+				char output[SERVER_SIZE] = "";
+
+				for(int i=starting_position; i < (starting_position+size); i++) {
+					printf("%d\n", i);
+					fflush(stdout);
+					output[i-starting_position] = server_database[i];
+					printf("%d\n\n", output[i-starting_position]);
+					fflush(stdout);
+				}
+
+				printf("\n server_database: %s\n", server_database);
+				fflush(stdout);
+
+				printf("\n output: %s\n", output);
+				fflush(stdout);
+
+				write(client_sockfd, &output, SERVER_SIZE);
+				sleep(1);
+			}
 		}
+		close(client_sockfd);
+	}
 
-		// [ ] - Le(Posicao4, tamanho)
-		char starting_position_arg[100] = "";
-		char size_arg[100] = "";
-		int starting_position, size;
-
-		read(client_sockfd, &starting_position_arg, 100);
-		printf("\n starting_position_arg: %s\n", starting_position_arg);
-		fflush(stdout);
-
-		read(client_sockfd, &size_arg, 100);
-		printf("\n size_arg: %s\n", size_arg);
-		fflush(stdout);
-
-		starting_position = atoi(starting_position_arg);
-        size = atoi(size_arg);
-
-		char output[SERVER_SIZE] = "";
-
-		for(int i=starting_position; i < (starting_position+size); i++) {
-			printf("%d\n", i);
-			fflush(stdout);
-			output[i-starting_position] = server_database[i];
-			printf("%d\n\n", output[i-starting_position]);
-			fflush(stdout);
-		}
-
-		printf("\n server_database: %s\n", server_database);
-		fflush(stdout);
-
-		printf("\n output: %s\n", output);
-		fflush(stdout);
-
-		write(client_sockfd, &output, SERVER_SIZE);
-        sleep(1);
-     }
- 	 close(client_sockfd);
- 
-     return 0;
+    return 0;
 }
